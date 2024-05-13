83215dc6c458ea31f157218b86dd47da55fab709
Ruben Salazar <rubensalazar@rocketmail.com>
2024-05-12 22:21:09 -0500

    funcitons lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (block) {
+    
+     if (block > 42){
+        return (block - 42)
+        
+     }else {
+        return (42 - block)
+     }
+    }
+
+
+function  distanceFromHqInFeet(block){
+    return distanceFromHqInBlocks(block) * 264
+    
+    
+  
+}
+  function distanceTravelledInFeet(start, destination) {
+        return Math.abs((start - destination) * 264);
+      }
+      
+      function calculatesFarePrice(start, destination){
+        const distanceTravelled = distanceTravelledInFeet(start, destination)
+            if (distanceTravelled < 400){
+                return(0)
+            }else if (distanceTravelled >= 400 && distanceTravelled <= 2000){
+                return (distanceTravelled -400)* .02
+            } else if (distanceTravelled >= 2000 && distanceTravelled <=2500){
+                return (25)
+                }else if (distanceTravelled >2500){
+                    return ("cannot travel that far")
+                }
+            } 
+        
+    
+      
+       
+
+        
+        
+        
+    
